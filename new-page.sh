4763a4285b951075485143514ae7a5ebf7d3ba89
Etherald <web@etherald.com>
2014-06-03 20:09:55 -0400

    add new-page.sh

---
+++ new-page.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# page-template.sh
+# part of phpsqlitesite
+# http://phpsqlitesite.com
+
+# runs sqlite sql query to add new page to db
+
+TITLE="$1"
+LABEL="$2"
+read "CONTENT"
+
+sqlite3 ./demo.sqlite <<EOF
+UPDATE "index" set title="$TITLE", label="$LABEL", content="$CONTENT";
+EOF
